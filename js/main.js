de7863a9b209f69e2b1ade1c223f78103edefd10
adiynil <aadiynil@gmail.com>
2021-10-28 10:25:28 +0000

    Deploying to gh-pages from @ adiynil/adiynil.github.io@ec4eda35ad8b34c6d84d2ca91f89f138353fa541 🚀

--- js/main.js
+++ js/main.js
@@ -1,34 +1,48 @@
-$(function(){
-	secondary = $( '#secondary' );
-	button = $( '.site-branding' ).find( '.secondary-toggle' );
-
-	// Enable menu toggle for small screens.
-	( function() {
-		var menu, widgets, social;
-		if ( ! secondary || ! button ) {
-			return;
-		}
-
-		button.on( 'click', function() {
-			secondary.toggleClass( 'toggled-on' );
-			secondary.trigger( 'resize' );
-			$( this ).toggleClass( 'toggled-on' );
-			if ( $( this, secondary ).hasClass( 'toggled-on' ) ) {
-				$( this ).attr( 'aria-expanded', 'true' );
-				secondary.attr( 'aria-expanded', 'true' );
-			} else {
-				$( this ).attr( 'aria-expanded', 'false' );
-				secondary.attr( 'aria-expanded', 'false' );
-			}
-		} );
-	} )();
-	
-	footer = $( '#infinite-footer' );
-	setInterval( function() {
-			// Reveal footer
-		if ( $(window).scrollTop() >= 350 )
-			footer.animate( { 'bottom': 0 }, 'fast' );
-		else if ( $(window).scrollTop() < 350 )
-			footer.animate( { 'bottom': '-50px' }, 'fast' );
-		}, 250 );
+$(function() {
+  const secondary = $('#secondary');
+  const button = $('.site-branding').find('.secondary-toggle')[0];
+  // Enable menu toggle for small screens.
+  (function() {
+    if (!secondary || !button) {
+      return;
+    }
+    button.addEventListener('click', function() {
+      secondary.toggleClass('toggled-on');
+      secondary.trigger('resize');
+      $(this).toggleClass('toggled-on');
+      if ($(this, secondary).hasClass('toggled-on')) {
+        $(this).attr('aria-expanded', 'true');
+        secondary.attr('aria-expanded', 'true');
+      } else {
+        $(this).attr('aria-expanded', 'false');
+        secondary.attr('aria-expanded', 'false');
+      }
+    });
+  })();
+  const backTop = $('#back-top')[0];
+  (() => {
+    if (!backTop) return;
+    const scrollTopSmooth = function(position) {
+      if (!window.requestAnimationFrame) {
+        window.requestAnimationFrame = function(cb) {
+          return setTimeout(cb, 10);
+        };
+      }
+      var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+      var step = function() {
+        var distance = position - scrollTop;
+        scrollTop = scrollTop + distance / 5;
+        if (Math.abs(distance) < 1) {
+          window.scrollTo(0, position);
+        } else {
+          window.scrollTo(0, scrollTop);
+          requestAnimationFrame(step);
+        }
+      };
+      step();
+    }
+    backTop.addEventListener('click', () => {
+      scrollTopSmooth(0);
+    })
+  })();
 })
